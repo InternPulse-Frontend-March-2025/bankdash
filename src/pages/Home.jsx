76b5a4f9a0a562f56a779ba817844f477611e132
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-03-30 09:29:15 +0100

    Revert "Implement Home"
    This reverts commit f335c6b9cb9dce35b180e9417f1bfca16e6c5787.
    
    Merge was wrong.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import WeeklyActivity from "../components/WeeklyActivity";
-import ExpenseStatistics from "../components/ExpenseStatistics";
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="h-screen w-full max-w-[1440px] bg-[#F5F7FA] mx-auto">
-      <p className="text-red-600 mb-6">This is the overview page</p>
-      <div className="p-2 grid sm:grid-cols-3 gap-4 h-auto">
-        <WeeklyActivity className="sm:col-span-2 h-[285px] md:h-[299px] xl:h-[367px] " />
-        <ExpenseStatistics className="w-full h-[271px] sm:h-[285px] md:h-[299px] xl:h-[367px]" />
-      </div>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
